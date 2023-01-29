690a9b454b49794c4345a4327c11645bc28c328f
dev-promtheus <ozh98@protonmail.com>
2023-01-29 11:04:48 +0000

    add softDeletes columns

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,10 +6,11 @@ use App\Models\Post;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
